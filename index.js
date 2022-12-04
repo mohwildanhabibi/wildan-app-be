c0e2a2d100713100def7a8c65f1cc7d4f2bffab0
Moh. Wildan Habibi <mohwildannhabibi@gmail.com>
2022-12-04 23:52:55 +0700

    add repo checker

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 require("module-alias/register");
-require("./db/db");
+require("~/db/db");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 // const auth = require("./controllers/authController");
 const clusterController = require("./controllers/clusterController");
+
+app.get("/", async (req, res, next) => {
+  try {
+    res.send("ALIVE!!!");
+  } catch (error) {
+    next(error);
+  }
+});
 
 // app.use(express.json());
 // app.use("/api/auth", auth);
