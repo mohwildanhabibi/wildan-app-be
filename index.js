33d6c0660eb1d40c7a1bbc1d1e8138b754a9c21d
Moh. Wildan Habibi <mohwildannhabibi@gmail.com>
2022-12-30 09:43:43 +0700

    feat: add my-ip API

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ app.get("/", async (req, res, next) => {
   }
 });
 
+app.get("/my-ip", async (req, res, next) => {
+  try {
+    const ipAddress = req.socket.remoteAddress;
+    res.send(ipAddress);
+  } catch (error) {
+    next(error);
+  }
+});
+
 // app.use(express.json());
 // app.use("/api/auth", auth);
 app.use("/ngaji", clusterController);
